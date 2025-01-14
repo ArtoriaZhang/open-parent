2d594978e84664a732e8aa556841eed4a44c66f3
Arthur Zhang <zhangwfkl@gmail.com>
2025-01-14 14:20:41 +0800

    Feat: utils: Add time format

--- common-utils/src/main/java/com/ilbluesky/common/utils/date/DateUtils.java
+++ common-utils/src/main/java/com/ilbluesky/common/utils/date/DateUtils.java
@@ -1,8 +1,14 @@
 package com.ilbluesky.common.utils.date;
 
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author Orca Zhang
  * date 12/10/2024
  */
 public class DateUtils {
+
+    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 }
