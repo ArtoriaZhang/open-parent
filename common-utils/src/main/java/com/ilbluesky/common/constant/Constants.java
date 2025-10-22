3847f7b4a3502eeb13d6a2205b15e9f8cc881bc9
Arthur Zhang <zhangwfkl@gmail.com>
2025-10-22 11:05:52 +0800

    Feat: Utils: Add common constant

---
+++ common-utils/src/main/java/com/ilbluesky/common/constant/Constants.java
@@ -0,0 +1,13 @@
+package com.ilbluesky.common.constant;
+
+/**
+ * @author Orca Zhang
+ * date 10/22/2025
+ */
+public interface Constants {
+
+    int YES = 1;
+    int NO = 0;
+    String YES_S = "1";
+    String NO_S = "0";
+}
